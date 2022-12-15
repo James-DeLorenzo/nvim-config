684073e4ce219e88c318155559734b51b15a6e53
James DeLorenzo <james.delorenzo@health-union.com>
2022-12-15 10:09:04 -0500

    add more colorcolumns, update some whitespace syntax

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,7 +1,7 @@
 vim.o.syntax = 'enable'
 vim.o.signcolumn = 'yes'
 vim.o.background = 'dark'
-vim.o.colorcolumn = "80,120"
+vim.o.colorcolumn = "80,100,120,200"
 vim.o.termguicolors = true
 
 vim.o.number = true
@@ -15,8 +15,8 @@ vim.o.wrap = false
 vim.o.scrolloff = 3
 vim.o.hidden = true
 vim.o.cmdheight = 2
-vim.opt.iskeyword:append({'-'})
-vim.opt.nrformats:append({'unsigned'})
+vim.opt.iskeyword:append({ '-' })
+vim.opt.nrformats:append({ 'unsigned' })
 -- tab stuffs
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
@@ -25,17 +25,16 @@ vim.o.expandtab = true
 vim.o.cursorline = true
 
 vim.o.confirm = true
-vim.opt.completeopt = {'menu', 'menuone', 'preview', 'noselect', 'noinsert'}
+vim.opt.completeopt = { 'menu', 'menuone', 'preview', 'noselect', 'noinsert' }
 
 -- whitespace chars
-vim.opt.listchars = {tab=">-<",multispace=""}
+vim.opt.listchars = { tab = ">-<", multispace = "", extends = ">", precedes = "<" }
 vim.o.list = true
 vim.o.modeline = true
 vim.o.foldlevelstart = 99
-vim.opt.formatoptions:remove({'o'})
+vim.opt.formatoptions:remove({ 'o' })
 
 vim.o.updatetime = 300
 vim.o.timeoutlen = 500
 
 vim.o.laststatus = 3
-
